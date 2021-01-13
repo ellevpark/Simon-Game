ea65858d4feee83718d0ad80d37dfe1bec61adb8
Ellen Park <ellenpark781@gmail.com>
2021-01-13 14:34:56 -0500

    created function called nextSequence to flash the color block

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-alert("I am working!")
+const buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random()*4);
+  let randomChosenColor = buttonColors[randomNumber];
+  console.log(randomChosenColor)
+
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor).fadeIn(500).fadeOut(500).fadeIn(500);
+  let audio = new Audio("/sounds/" + randomChosenColor + ".mp3");
+  console.log(audio)
+  audio.play();
+  };
+
+
+nextSequence();
+
+
+
+
