36882c1ad5cbafc87485b93dc6b2af7182436fbd
Ellen Park <ellenpark781@gmail.com>
2021-01-13 14:49:33 -0500

    created an array to keep track of user clicks

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random()*4);
@@ -9,12 +10,15 @@ function nextSequence() {
   gamePattern.push(randomChosenColor);
   $("#" + randomChosenColor).fadeIn(500).fadeOut(500).fadeIn(500);
   let audio = new Audio("/sounds/" + randomChosenColor + ".mp3");
-  console.log(audio)
   audio.play();
   };
 
+  $(".btn").on("click", function() {
+    let userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern)
+  })
 
-nextSequence();
 
 
 
