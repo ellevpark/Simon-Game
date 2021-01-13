4bbc487c3558c2e7cecf42ba926f6870125ce413
Ellen Park <ellenpark781@gmail.com>
2021-01-13 15:04:39 -0500

    played sound whenever user clicks a colored square

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+
+
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
@@ -8,18 +10,21 @@ function nextSequence() {
   console.log(randomChosenColor)
 
   gamePattern.push(randomChosenColor);
-  $("#" + randomChosenColor).fadeIn(500).fadeOut(500).fadeIn(500);
-  let audio = new Audio("/sounds/" + randomChosenColor + ".mp3");
-  audio.play();
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor)
   };
 
   $(".btn").on("click", function() {
     let userChosenColor = this.id;
     userClickedPattern.push(userChosenColor);
     console.log(userClickedPattern)
+    playSound(userChosenColor);
+    nextSequence();
   })
 
-
-
+function playSound(colorName) {
+  let audio = new Audio("sounds/" + colorName + ".mp3");
+  audio.play();
+}
 
 
