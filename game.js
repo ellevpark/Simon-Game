fca44a06e04afdfdf98446dabc8c0502955e0403
Ellen Park <ellenpark781@gmail.com>
2021-01-19 12:30:51 -0500

    created variable level and initialized it to 0, increments after every click

--- game.js
+++ game.js
@@ -3,16 +3,7 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
-
-function nextSequence() {
-  let randomNumber = Math.floor(Math.random()*4);
-  let randomChosenColor = buttonColors[randomNumber];
-  console.log(randomChosenColor)
-
-  gamePattern.push(randomChosenColor);
-  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColor)
-  };
+let level = 0;
 
   $(".btn").on("click", function() {
     let userChosenColor = this.id;
@@ -20,8 +11,21 @@ function nextSequence() {
     console.log(userClickedPattern)
     playSound(userChosenColor);
     animatePress(userChosenColor);
+    $("#level-title").text("Level " + level);
+    nextSequence();
   })
 
+  function nextSequence() {
+    let randomNumber = Math.floor(Math.random()*4);
+    let randomChosenColor = buttonColors[randomNumber];
+    console.log(randomChosenColor)
+  
+    gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor)
+    $("#level-title").text("Level " + level++);
+    };
+  
 function playSound(colorName) {
   let audio = new Audio("sounds/" + colorName + ".mp3");
   audio.play();
