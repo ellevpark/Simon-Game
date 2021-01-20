ada34cd0a5b4b9c943b0a2af783c048b4da75ad3
Ellen Park <ellenpark781@gmail.com>
2021-01-20 11:05:27 -0500

    ended game when user chooses wrong sequence

--- game.js
+++ game.js
@@ -1,37 +1,68 @@
-
-
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
 let level = 0;
+let started = false; 
 
-  $(".btn").on("click", function() {
-    let userChosenColor = this.id;
-    userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern)
-    playSound(userChosenColor);
-    animatePress(userChosenColor);
+$(document).keypress(function() {
+  if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
+    started = true;
+  }
+});
+
+  $(".btn").click(function() {
+    let userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length-1);
   })
 
-  function nextSequence() {
-    let randomNumber = Math.floor(Math.random()*4);
-    let randomChosenColor = buttonColors[randomNumber];
-    console.log(randomChosenColor)
-  
-    gamePattern.push(randomChosenColor);
-    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColor)
-    $("#level-title").text("Level " + level++);
-    };
-  
+function nextSequence() {
+  userClickedPattern = [];
+  level++
+  $("#level-title").text("Level " + level);
+
+  let randomNumber = Math.floor(Math.random()*4);
+  let randomChosenColor = buttonColors[randomNumber];
+
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor)
+  };
+
 function playSound(colorName) {
   let audio = new Audio("sounds/" + colorName + ".mp3");
   audio.play();
 }
 
 function animatePress(currentColor) {
-  $("#" + currentColor).addClass("pressed").delay(100).removeClass("pressed")
+  $("#" + currentColor).addClass("pressed"); 
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed")
+  }, 100)
 }
 
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success!")
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+    },1000);
+  }
+   
+  } else {
+    console.log("wrong")
+    let wrongSound = new Audio("sounds/wrong.mp3");
+    wrongSound.play()
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over")
+    }, 200)
+    $("#level-title").text("Game Over, Press Any Key to Restart")
+  }
+
+}
